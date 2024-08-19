ea72cb9ff75de1987e2e5391ba34c6a64d5308d7
qianzhong516 <zhongqian516@gmail.com>
2024-08-19 12:14:58 +0200

    add focusOnError to the input field

--- src/PasswordInput.js
+++ src/PasswordInput.js
@@ -1,30 +1,55 @@
-import { useId, useState, memo } from 'react';
+import { useId, useState, memo, forwardRef, useImperativeHandle, useRef } from 'react';
 import { twMerge, twJoin } from 'tailwind-merge';
 import { TickIcon } from './TickIcon';
 
-export default function PasswordInput({
+const PasswordInput = forwardRef(({
     label,
     field,
     placeholder,
     className,
     value,
     onChange,
-    isValid
-}) {
+    isValid,
+    required,
+}, ref) => {
     const id = useId();
     const [isMasked, setIsMasked] = useState(true);
+    const [isErrored, setIsErrored] = useState(false);
     const toggleMaskPassword = () => setIsMasked(state => !state);
+    const inputRef = useRef(null);
 
     const Icon = memo(({ isActive }) => {
         const commonClasses = 'cursor-pointer text-neutral-400';
         return isActive ? <i className={twJoin(commonClasses, 'ri-eye-close-line')}></i> : <i className={twJoin(commonClasses, 'ri-eye-line')}></i>
     });
 
+    const handleOnBlur = () => setIsErrored(false);
+
+    useImperativeHandle(ref, () => {
+        return {
+            focusOnError: () => {
+                setIsErrored(true);
+                inputRef.current.focus();
+            }
+        }
+    }, []);
+
     return (
         <div className='flex flex-col gap-1'>
             <label className="text-sm" htmlFor={id}>{label}</label>
             <div className='relative'>
-                <input className={twMerge('w-full p-2 border rounded-md text-sm', className)} type={isMasked ? 'password' : 'text'} name={field} id={id} placeholder={placeholder} value={value} onChange={onChange} />
+                <input
+                    className={twMerge('w-full p-2 border rounded-md text-sm bg-neutral-100 placeholder:text-neutral-500', isErrored && 'focus:outline-red-400', className)}
+                    type={isMasked ? 'password' : 'text'}
+                    name={field}
+                    id={id}
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={onChange}
+                    required={required}
+                    ref={inputRef}
+                    onBlur={handleOnBlur}
+                />
                 <div className='absolute top-2 right-2' onClick={toggleMaskPassword} >
                     <div className='flex gap-2 items-center'>
                         {isValid && <TickIcon isActive={isValid} />}
@@ -34,4 +59,6 @@ export default function PasswordInput({
             </div>
         </div>
     )
-}
+})
+
+export default PasswordInput;
